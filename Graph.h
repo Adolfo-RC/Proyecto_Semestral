cb5b01901f23be0e83f27408d781bd440c77b6d8
Adolfo-RC <github.user>
2021-07-28 10:49:46 -0400

    Añadidos comentarios

--- Graph.h
+++ Graph.h
@@ -1,3 +1,5 @@
+// Cabecera de la estructura nodal y la clase grafo.
+
 #ifndef PROYECTO_FINAL_GRAPH_H
 #define PROYECTO_FINAL_GRAPH_H
 
@@ -11,73 +13,74 @@
 #include <stack>
 
 using namespace std;
-//namesppcese to bue used for typename simplification
-using uint = unsigned int;
+
+using uint = unsigned int; 
 using str = string;
 using umap = unordered_map<str, int>;
 
-// Cointainer node struct
+// Estructura Nodo.
 struct Node {
-    str id; // ID field
-    int pos;
-    int following;
-    double polPow;
-    deque<Node *> followers;
-    vector<double> politicalTendency; // vector of political tendency with 4 components (1 for each tendency)
-
+    str id; // Nombre de usuario.
+    int pos; // Posición en el dataset.
+    int following; // Cantidad de personas que sigue el usuario.
+    double polPow; // Tendencia política valor numérico.
+    deque<Node *> followers; // Lista de seguidores como nodos.
+    vector<double> politicalTendency; 
+    // Vector que mantiene las 4 posturas políticas con sus propios valores.
 };
 
-
+// Clase Grafo.
 class Graph {
 private:
-    umap ref; // hash table to associate the ID field with the position in the list
-    //unordered_map<int, str> inverse_ref; // From position to ID
-    vector<Node> graph; //adjacency list
-    void politicalTendencyCalc(str Magazine);
+    umap ref; // Tabla hash que asocia nombre de usuario con posición.
+    vector<Node> graph; // Vector de adyacencia entre nodos.
+
+    void politicalTendencyCalc(str Magazine); 
+    // Función que añade el valor de la tendencia política respectiva al noticiero y a 
+    // la posicion del seguidor en el grafo.
 
     void DFSUtil (int n, bool visited []);
+    // Función que realiza la busqueda por profundidad necesaria para Kosaraju.
 
     void fillOrder(int n, bool visited [], stack<Node> &s);
+    // Función que rellena la lista de nodos para Kosaraju.
 
-    Graph transpose();
+    Graph transpose(); // Invierte los nodos del grafo.
 
-    void insert(Node, Node);
+    void insert(Node, Node); // Inserta los nodos a otros nodos.
 
 public:
-    Graph(); // Class constructor
-    void insert(str vertex, str junctions); // insert in graph
-    void insert(str node);
+    Graph(); // Constructor.
+    ~Graph(); // Destructor.
 
+    void insert(str vertex, str junctions); // Inserta al grafo nodos y arcos.
+    void insert(str node); // Inserta al grafo nodos.
 
+    void print(); // Imprime el grafo.
+    
+    vector<Node> topInfluencer(int n); // Entrega los "n" usuarios con más seguidores.
 
-    void print(); // print graph
-    vector<Node> topInfluencer(int n); // returns a ranking with the most followed users
+    vector<Node> topInfluenced(int n); // Entrega los "n" usuarios que siguen a más cuentas.
 
-    vector<Node> topInfluenced(int n); // returns a ranking with the most followers users
+    vector<Node> influenceColorMap(); // Entrega un arreglo porcentual de la influencia 
+                                      // política de cada usuario.
 
-    vector<Node> influenceColorMap(); // returns an array with % of political influence for every user
+    int Kosaraju(); // Algoritmo de Kosaraju que entrega las componentes fuertemente conexas.
 
-    int Kosaraju(); // Kosaraju's SCC detection algorithm. Returns count
+    void exportGraph(str path); // Exporta el grafo como archivo de texto.
 
-    void exportGraph(str path); // Export graph as a txt
+    void exportTendencies(str path); // Exporta las tendencias políticas como archivo de texto.
 
-    void exportTendencies(str path); // Export tendencies as txt
+    void computeStats(); // Calcula la totalidad de la influencia política entre los usuarios.
 
-    void computeStats(); // compute the political dispersion in Chile
+    int size(); // Entrega la cantidad de nodos.
 
-    int size(); // returns the number of vertex in graph
+    Node returnNode(int n); // Entrega un nodo según su posición.
+    Node returnNode(str id); // Entrega un nodo según su nombre de usuario.
 
-    Node returnNode(int n); // returns a partiular node
-    Node returnNode(str id); // returns a particular node str overloaded
+    void getGraphSize(); // Calcula el tamaño total del grafo.
 
-    void getGraphSize(); // Computes the graph size in MB
-
-    int findIsland();
-
-
-    ~Graph(); // class destructor
-
+    int findIsland(); // Identifica nodos que no tengan arcos.
 };
-
 
 #endif //PROYECTO_FINAL_GRAPH_H
--- main.cpp
+++ main.cpp
@@ -38,42 +38,57 @@ int main() {
     // Se calcula la diferencia sobre segundos para obtener el tiempo transcurrido.
     
     cout << "Influence by components\n";
+    // Se calcula la influencia de los noticieros en los usuarios.
     t0 = clock();
     vector<Node> k = tweet.influenceColorMap();
     tf = clock();
     cout << "\n\nTime to calculate influence: " << (tf - t0) / CLOCKS_PER_SEC << endl << endl;
+    // Tiempo transcurrido.
 
     cout << "Top 10 Influenced\n";
+    // Se calculan los 10 usuarios más influenciados.
     t0 = clock();
     auto z = tweet.topInfluenced(10);
     tf = clock();
     cout << "\n\nTime to calculate influenceds: " << (tf - t0) / CLOCKS_PER_SEC << endl << endl;
+    // Tiempo transcurrido.
 
     for (auto i : z){
+    // Se imprime los nombres y la cantidad de gente que siguen.
         cout << i.id << " " << i.following << endl;
     }
-    cout << "Top 10 Influenced\n";
+
+    cout << "Top 10 Influencers\n";
+    // Se calculan los 10 usuarios más influyentes.
     t0 = clock();
     auto y = tweet.topInfluencer(10);
     tf = clock();
     cout << "\n\nTime to calculate influencers: " << (tf - t0) / CLOCKS_PER_SEC << endl << endl;
+    // Tiempo transcurrido.
 
     for (auto i : y){
+    // Se imprimen los nombres y la cantidad de gente que los siguen.
         cout << i.id << " " << i.followers.size() << endl;
     }
+
     cout << "SCC Detection\n";
+    // Se calculan los conjuntos fuertemente conexos mediante Kosaraju.
     t0 = clock();
     int x = tweet.Kosaraju();
     tf = clock();
     cout << "\n\nTime to calculate influence: " << (tf - t0) / CLOCKS_PER_SEC << endl << endl;
+    // Tiempo transcurrido.
 
     cout << endl << endl << " Number of SCCs"<< x << endl;
+    // Cantidad de componentes fuertemente conexas incluyendo singularidades.
+
     cout << "Stats\n";
+    // Porcentajes totales de la influencia de los usuarios.
     tweet.computeStats();
 
     cout << "Graph size\n";
+    // Tamaño total del grafo.
     tweet.getGraphSize();
-
 
     return 0;
 }
